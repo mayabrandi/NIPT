6ba6e15c1f9a92f550df1d510ec79aec65397185
Maya Brandi <mayabrandi@130-229-14-72-dhcp.wlan.ki.se>
2016-03-02 14:59:36 +0100

    ??

--- extentions.py
+++ extentions.py
@@ -9,11 +9,28 @@ import sys
 import glob
 from flask.ext.login import LoginManager
 from flask_oauthlib.client import OAuth
+from flask_sslify import SSLify
+from OpenSSL import SSL
+
+# (ext lacks init_app...)
+ctx = SSL.Context(SSL.SSLv23_METHOD)
+
+def ssl(app):
+    # Setup SSL: http://flask.pocoo.org/snippets/111/
+    ctx.use_privatekey_file(app.config.get('SSL_KEY_PATH'))
+    ctx.use_certificate_file(app.config.get('SSL_CERT_PATH'))
+
+    # https://github.com/kennethreitz/flask-sslify
+    # Force SSL. Redirect all incoming requests to HTTPS.
+    # Only takes effect when DEBUG=False
+    return SSLify(app)
+
 
 
 app = Flask(__name__)
 app.config.from_pyfile('../../config/config.py')
 mail = Mail(app)
+ssl(app)
 
 login_manager = LoginManager(app)
 oauth = OAuth(app)
