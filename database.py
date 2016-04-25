9660b25990932bfcbf5914695ef804294887c0a0
Maya Brandi <mayabrandi@130-229-0-73-dhcp.wlan.ki.se>
2016-04-25 14:49:14 +0200

    version

--- database.py
+++ database.py
@@ -310,7 +310,7 @@ class NCV(db.Model):
         self.NCD_21 = nipt_dict['NCD_21']
         self.NCD_X = nipt_dict['NCD_X']
         self.NCD_Y = nipt_dict['NCD_Y']
-        self.include = True             # set to False by default before handing over!!!
+        self.include = False             # set to False by default before handing over!!!
         self.change_include_date = ''
         self.comment = ''
 
