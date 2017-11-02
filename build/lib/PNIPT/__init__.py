7e59fdc79aa1ec64f360c24eaddf3a346c0f4aa1
hiseq clinical <hiseq.clinical@clinical-db.scilifelab.se>
2017-11-02 08:51:55 +0100

    PNIPT 173110

---
+++ build/lib/PNIPT/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.mail import Mail
+from flask.ext.login import LoginManager
+from flask_oauthlib.client import OAuth
+from flask_sslify import SSLify
+import ssl
+from werkzeug.contrib.fixers import ProxyFix
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py')
+
+import PNIPT.views
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+
+
+# the toolbar is only enabled in debug mode:
+#app.debug = True
+
+# set a 'SECRET_KEY' to enable the Flask session cookies
+#app.config['SECRET_KEY'] = 'hemlignyckel'
+
+#toolbar = DebugToolbarExtension(app)
